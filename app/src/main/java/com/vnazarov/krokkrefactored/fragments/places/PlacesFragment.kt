54e9fe9fdcc92d827bb6a06e63dfb60b1e11df42
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-02-19 21:45:12 +0300

    Places rv initialized

--- app/src/main/java/com/vnazarov/krokkrefactored/fragments/places/PlacesFragment.kt
+++ app/src/main/java/com/vnazarov/krokkrefactored/fragments/places/PlacesFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vnazarov.krokkrefactored.MainActivity
 import com.vnazarov.krokkrefactored.R
 import com.vnazarov.krokkrefactored.databinding.FragmentPlacesBinding
@@ -16,6 +18,8 @@ class PlacesFragment: Fragment() {
 
     private lateinit var mBinding: FragmentPlacesBinding
     private lateinit var mToolbar: Toolbar
+    private lateinit var adapter: PlacesAdapter
+    private lateinit var mRecyclerView: RecyclerView
     private val viewModel: PlacesViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -27,6 +31,9 @@ class PlacesFragment: Fragment() {
         mToolbar = mBinding.placesToolbar
         viewModel.initializeToolbar(activity as MainActivity, mToolbar, "Places")
 
+        mRecyclerView = mBinding.placesRv
+        mRecyclerView.layoutManager = LinearLayoutManager(activity)
+
         return mBinding.root
     }
 
@@ -35,8 +42,8 @@ class PlacesFragment: Fragment() {
 
         viewModel.defineNavController(view)
 
-        mBinding.textView3.setOnClickListener {
-            viewModel.navigateTo(R.id.action_placesFragment_to_currentPlaceFragment, fromName = "places")
-        }
+        val dataPlaces = viewModel.initializePlaces()
+        adapter = PlacesAdapter(dataPlaces, viewModel)
+        mRecyclerView.adapter = adapter
     }
 }
